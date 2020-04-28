066181ac467a412eefe1a5117b4a95791bff7846
al3xlapo <al3xlapo@gmail.com>
2020-04-28 17:18:53 +0300

    Add files via upload

---
+++ precache-manifest.9b3319d1ec295e83b5487e2ef518434b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1797c1a1800617b94f4d7379a590d65c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b698a0d50d76f37180cf",
+    "url": "./static/css/main.8c73b249.chunk.css"
+  },
+  {
+    "revision": "d3c95d5e284e5ef0464e",
+    "url": "./static/js/2.45301d41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.45301d41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b698a0d50d76f37180cf",
+    "url": "./static/js/main.f527b9fb.chunk.js"
+  },
+  {
+    "revision": "9f83b26a1db4959e588c",
+    "url": "./static/js/runtime-main.389ed694.js"
+  },
+  {
+    "revision": "687c5c4eb7be2549ddb63c44e7a76f4c",
+    "url": "./static/media/808.687c5c4e.wav"
+  },
+  {
+    "revision": "9ece8ab8438b99c33eef24ca56ceabb2",
+    "url": "./static/media/Clap.9ece8ab8.wav"
+  },
+  {
+    "revision": "f8ab98108344d1a30d144470fe7d3176",
+    "url": "./static/media/Crash.f8ab9810.wav"
+  },
+  {
+    "revision": "a7941fdf8da5146db2a55b1f9127572a",
+    "url": "./static/media/Hihat.a7941fdf.wav"
+  },
+  {
+    "revision": "528d508867c15f6b6fbd37f25a363b8e",
+    "url": "./static/media/Kick.528d5088.wav"
+  },
+  {
+    "revision": "01638a64acf1a57437d72c6d99e9e074",
+    "url": "./static/media/Open-Hat.01638a64.wav"
+  },
+  {
+    "revision": "cff7c462df8e1d3d77d6ba886ffc318d",
+    "url": "./static/media/Perc.cff7c462.wav"
+  },
+  {
+    "revision": "143d199629b9cab362a58c44e17498a6",
+    "url": "./static/media/Perc2.143d1996.wav"
+  },
+  {
+    "revision": "79aba21f857916c016248737aa380060",
+    "url": "./static/media/Snare.79aba21f.wav"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
